e6d935e3e7256a024032993588b44dc7f6c071ce
amal <amalgiressh007@gmail.com>
2025-10-12 10:31:34 +0530

    home work day 2

---
+++ hwork2.sql
@@ -0,0 +1,4 @@
+SELECT * FROM library WHERE price > 400;
+SELECT * FROM library WHERE genre IN ('History', 'Science', 'Fiction');
+SELECT * FROM library WHERE title = ('The Great Gatsby');
+SELECT * FROM library WHERE author <> 'Dan Brown';
