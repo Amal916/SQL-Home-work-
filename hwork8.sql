06497563619e3ff8e8d24a43b01c4ce21b61ff3e
Amal916 <amalgireesh007@gmail.com>
2025-10-26 20:11:54 +0530

    home work day 8

---
+++ hwork8.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE BookStoreDB;
+USE BookStoreDB;
+CREATE TABLE authors ( author_id INT PRIMARY KEY, name VARCHAR(100), country VARCHAR(100));
+CREATE TABLE books ( book_id INT PRIMARY KEY, title VARCHAR(150), price DECIMAL(10, 2), author_id INT, FOREIGN KEY (author_id) REFERENCES authors(author_id));
+ALTER TABLE books ADD COLUMN published_year INT;
+TRUNCATE TABLE books;
+DROP DATABASE BookStoreDB;
+
